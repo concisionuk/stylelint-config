1e03376565f0c942fddb6afbeb75cdd09e2aafb0
Daniel Roe <daniel@concision.co.uk>
2018-12-06 15:02:07 +0000

    Add Tailwind support

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+'use strict';
+
 module.exports = {
   extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
   plugins: ['stylelint-no-unsupported-browser-features'],
   rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['extends', 'tailwind', 'apply', 'responsive', 'variants', 'screen', 'include']
+      }
+    ],
+    'block-no-empty': true,
     'plugin/no-unsupported-browser-features': [
       true,
       {
-        severity: 'warning',
-      },
-    ],
-  },
-}
+        severity: 'warning'
+      }
+    ]
+  }
+};
